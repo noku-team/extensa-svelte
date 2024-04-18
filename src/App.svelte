4812e44ddb13350deeeb75e05495b7a381efb245
Luca Valsecchi <luca.valsecchi@noku.io>
2024-04-18 13:33:12 +0200

    feat(auth): Persist auth on page refresh

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script>
+	import { onMount } from "svelte";
 	import { Route, Router } from "svelte-routing";
 	import ExtensaHeader from "./lib/Header.svelte";
 	import User from "./lib/User.svelte";
 	import WebGl from "./lib/WebGL.svelte";
+	import { authStore } from "./store/AuthStore";
 
 	export let url = "";
+
+	onMount(() => {
+		// Persist auth on page refresh
+		authStore.sync();
+	});
 </script>
 
 <Router {url}>

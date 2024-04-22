7019c7b2b10a77959b2a808a8f409225acb740b9
Luca Valsecchi <luca.valsecchi@noku.io>
2024-04-22 11:39:02 +0200

    feat(modal): change modal layout for project preview

--- src/lib/SelectedProject.svelte
+++ src/lib/SelectedProject.svelte
@@ -2,18 +2,14 @@
 	import { VARCO } from "../VARCO/helpers/VARCO";
 	import { EDITOR, PLY, UI } from "../jsm";
 	import { projectStore } from "../store/ProjectStore";
-	import Modal from "./Modal.svelte";
 	import EyeOffIcon from "/images/UI/eye-off.png";
 	import EyeIcon from "/images/UI/eye.png";
-
-	let showModal = false;
-
-	$: showModal = !!$projectStore.project;
 
 	const onEyeClick = () => {
 		EDITOR.f.loadProjectData();
 		projectStore.set3DVisible(true);
 	};
+	
 	const onEyeOffClick = () => {
 		UI.p.previewProject.f.button_removeProject();
 		projectStore.set3DVisible(false);
@@ -40,8 +36,8 @@
 	};
 </script>
 
-<Modal top bind:showModal title="Project" {onClose}>
-	<div class="flex flex-col gap-3">
+{#if !!$projectStore.project}
+	<div class="flex flex-col gap-3 fixed top-20 left-1/2 p-10 bg-neutral z-10 rounded-xl">
 		<span class="font-semibold text-xl">{$projectStore.project?.name}</span>
 		<div class="flex">
 			{#if !$projectStore.is3DVisible}
@@ -51,6 +47,6 @@
 			{/if}
 		</div>
 	</div>
-</Modal>
+{/if}
 
 <style></style>

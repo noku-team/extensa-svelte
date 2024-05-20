ac68eec471c9b8c2576ea8b65b59abfeb2d2f608
TheBous <thebous1993@gmail.com>
2024-05-20 16:43:04 +0200

    fix(buttons): fix disabled condition

--- src/extensa_frontend/src/lib/MapButtons/MapButtons.svelte
+++ src/extensa_frontend/src/lib/MapButtons/MapButtons.svelte
@@ -10,17 +10,38 @@
 	import Rotate from "/images/UI/buttons/circle_left.png";
 	import Settings from "/images/UI/buttons/settings.png";
 
-	type ActiveId = "Move" | "Drop" | "Enlarge" | "Rotate" | "Folder" | "Settings";
+	type ActiveId =
+		| "Move"
+		| "Drop"
+		| "Enlarge"
+		| "Rotate"
+		| "Folder"
+		| "Settings";
 
 	let activeId: ActiveId | null = null;
 
+	enum ButtonType {
+		Drop = "Drop",
+		Folder = "Folder",
+		Rotate = "Rotate",
+		Move = "Move",
+		Enlarge = "Enlarge",
+		Settings = "Settings",
+	}
+
 	const buttons = [
-		{ src: Drop, alt: "Drop", id: "Drop" },
-		{ src: Folder, alt: "Folder", id: "Folder", disabled: true },
-		{ src: Rotate, alt: "Rotate", id: "Rotate" },
-		{ src: Move, alt: "Move", id: "Move" },
-		{ src: Enlarge, alt: "Enlarge", id: "Enlarge" },
-		{ src: Settings, alt: "Settings", id: "Settings" },
+		{
+			src: Drop,
+			alt: "Drop",
+			id: ButtonType.Drop,
+			enabled: true,
+			forceEnabling: true,
+		},
+		{ src: Folder, alt: "Folder", id: ButtonType.Folder, enabled: false },
+		{ src: Rotate, alt: "Rotate", id: ButtonType.Rotate },
+		{ src: Move, alt: "Move", id: ButtonType.Move },
+		{ src: Enlarge, alt: "Enlarge", id: ButtonType.Enlarge },
+		{ src: Settings, alt: "Settings", id: ButtonType.Settings },
 	];
 
 	const toggleActive = (id: ActiveId) => {
@@ -50,15 +71,19 @@
 </script>
 
 {#if $authStore.identity}
-	<div class="fixed left-2 top-1/2 transform -translate-y-1/2 z-[1000] flex flex-col gap-1">
-		{#each buttons as { src, alt, id, disabled = false }}
+	<div
+		class="fixed left-2 top-1/2 transform -translate-y-1/2 z-[1000] flex flex-col gap-1"
+	>
+		{#each buttons as { src, alt, id, enabled = true, forceEnabling = false }}
 			<Button
 				{src}
 				{alt}
 				active={activeId === id}
 				toggleActive={() => toggleActive(id)}
 				deselectBtn={() => (activeId = null)}
-				disabled={!disabled && !$projectStore.project && !PLY.p.selectedArea}
+				disabled={forceEnabling
+					? false
+					: !enabled || (!$projectStore.project && !PLY.p.selectedArea)}
 			/>
 		{/each}
 	</div>

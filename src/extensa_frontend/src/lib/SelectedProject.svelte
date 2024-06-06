ee3c7889105b73a8a027f4b7edc2401d1d51be77
TheBous <thebous1993@gmail.com>
2024-06-06 10:23:04 +0200

    fix(close): remove close button from unauth

--- src/extensa_frontend/src/lib/SelectedProject.svelte
+++ src/extensa_frontend/src/lib/SelectedProject.svelte
@@ -208,26 +208,28 @@
 						/>
 					</svg>
 				</button>
-				<button
-					class="btn btn-primary btn-circle cursor-pointer btn-sm"
-					on:click={onClose}
-					disabled={$projectStore.sendProjectProgress > 0 ||
-						$projectStore.loadProjectProgress > 0}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M6 18L18 6M6 6l12 12"
-						/></svg
+				{#if $authStore.identity}
+					<button
+						class="btn btn-primary btn-circle cursor-pointer btn-sm"
+						on:click={onClose}
+						disabled={$projectStore.sendProjectProgress > 0 ||
+							$projectStore.loadProjectProgress > 0}
 					>
-				</button>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							><path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M6 18L18 6M6 6l12 12"
+							/></svg
+						>
+					</button>
+				{/if}
 			</div>
 			<span class="font-bold text-2xl truncate max-w-full"
 				>{$projectStore.project?.name?.toUpperCase()}</span
